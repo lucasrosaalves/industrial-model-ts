cbf663a6891ec1bd7b71f40159397ff4792d16db
Leandro Almeida <lndr27@hotmail.com>
2026-09-19 13:26:50 -0300

    formatting

--- src/types.ts
+++ src/types.ts
@@ -124,12 +124,12 @@ type SortInput<TModel> = {
   deletedTime?: SortDirection;
   lastUpdatedTime?: SortDirection;
 } & (ModelInstanceType<TModel> extends "edge"
-  ? {
-      startNode?: SortDirection;
-      endNode?: SortDirection;
-      type?: SortDirection;
-    }
-  : {});
+    ? {
+        startNode?: SortDirection;
+        endNode?: SortDirection;
+        type?: SortDirection;
+      }
+    : {});
 
 export type QueryOptions<
   TModel,
